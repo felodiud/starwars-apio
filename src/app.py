b8b8603b2bf03324dd1dab63171d1f620d49c3e9
felipe neely <felodiud>
2023-08-09 01:14:57 -0400

    initial commit

--- src/app.py
+++ src/app.py
@@ -48,4 +48,4 @@ def handle_hello():
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=PORT, debug=False)
+    app.run(host='localhost', port=PORT, debug=False)
--- src/models.py
+++ src/models.py
@@ -1,19 +1,43 @@
 from flask_sqlalchemy import SQLAlchemy
 
+
+
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+#     password = db.Column(db.String(80), unique=False, nullable=False)
+#     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+#     def __repr__(self):
+#         return '<User %r>' % self.username
+
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             "email": self.email,
+#             # do not serialize the password, its a security breach
+#         }
+
+
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+class Character(db.Model):
+    __tablename__= 'character'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(200), nullable = False)
+    jedi = db.Column(db.Boolean, nullable = False)
+    origin = db.Column(db.String(200), nullable = False)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
-
-    def serialize(self):
+    def to_dict(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "name": self.name,
+            "origin": self.origin,
+            "jedi": self.origin,
         }
+    
+class Planet(db.Model):
+    __tablename__ = "planet"
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(200), nullable = False)
+    
